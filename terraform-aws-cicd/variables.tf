1a9fad7622e2e0bfd2747be09459ab88673355cb
30Piraten <victorehikioyaraeva@outlook.com>
2025-03-15 21:30:08 +0100

    update

--- terraform-aws-cicd/variables.tf
+++ terraform-aws-cicd/variables.tf
@@ -12,3 +12,13 @@ variable "github_repo_url" {
   type = string 
   default = "https://github.com/30Piraten/soleng.git"
 }
+
+variable "repo_id" {
+  type = string 
+  default = "30Piraten/soleng"
+}
+
+variable "codepipeline_name" {
+  type = string 
+  default = "codepipeline-v1"
+}
