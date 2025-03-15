1eb318720de54f2082e054750e1adc604bd0a655
30Piraten <victorehikioyaraeva@outlook.com>
2025-03-15 22:10:18 +0100

    Added policy_attachment for S3

--- terraform-aws-cicd/modules/codebuild/iam.tf
+++ terraform-aws-cicd/modules/codebuild/iam.tf
@@ -14,22 +14,13 @@ data "aws_iam_policy_document" "codebuild_assume_role_policy" {
 }
 
 // IAM Role for CodeBuild
-resource "aws_iam_role" "codebuild_s3_role" {
+resource "aws_iam_role" "codebuild_role" {
     name = "cicd-codebuild-role"
   assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role_policy.json
 }
 
 // IAM Policy for CodeBuild to interact with S3
-data "aws_iam_policy_document" "codebuild_s3_policy_doc" {
-  statement {
-    effect = "Allow"
-    actions = [ "s3:GetObject", "s3:PutObject", "s3:ListBucket" ]
-    resources = [
-      "arn:aws:s3:::${var.s3_bucket_id}", # Bucket level
-      "arn:aws:s3:::${var.s3_bucket_id}/*", # Object level
-    ]
-  }
-
+data "aws_iam_policy_document" "codebuild_policy_doc" {
   statement {
     effect = "Allow"
     actions = [ "secretsmanager:GetSecretValue" ]
@@ -47,13 +38,19 @@ data "aws_iam_policy_document" "codebuild_s3_policy_doc" {
   }
 }
 
-// Attach the policy to the CodeBuild role
-resource "aws_iam_policy" "codebuild_s3_policy" {
-  name = "codebuild-s3-policy"
-  policy = data.aws_iam_policy_document.codebuild_s3_policy_doc.json
+// Attach the core policy
+resource "aws_iam_policy" "codebuild_policy" {
+  name = "codebuild-policy"
+  policy = data.aws_iam_policy_document.codebuild_policy_doc.json
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_s3_attachment" {
-  role = aws_iam_role.codebuild_s3_role.name  
-  policy_arn = aws_iam_policy.codebuild_s3_policy.arn 
+resource "aws_iam_role_policy_attachment" "codebuild_attachment" {
+  role = aws_iam_role.codebuild_role.name  
+  policy_arn = aws_iam_policy.codebuild_policy.arn   
+}
+
+// Attach the shared S3 policy
+resource "aws_iam_role_policy_attachment" "codebuild_s3_access" {
+  role = aws_iam_role.codebuild_role.name 
+  policy_arn = var.s3_policy_arn
 }
